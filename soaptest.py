d4d12afe14acdb81d4469e4d5aabccbb253e0c36
Билолиддин Корабойев <b.koraboyev@napm.uz>
2019-11-04 11:57:46 +0500

    first commitss

--- soaptest.py
+++ soaptest.py
@@ -1,7 +1,7 @@
-from suds.client import Client
-url = 'https://ips.gov.uz:443/mediate/ips/STC/GetTinbyPasNum?wsdl'
-client = Client(url)
+#from suds.client import Client
+#url = 'https://ips.gov.uz:443/mediate/ips/STC/GetTinbyPasNum?wsdl'
+#client = Client(url)
 
-result = client.service['GetTinbyPasNumSOAP12BindingPort'].rEQ(pasSer='AA',pasNum='2550708',lang=1 )
+#result = client.service['GetTinbyPasNumSOAP12BindingPort'].rEQ(pasSer='AA',pasNum='2550708',lang=1 )
 
-print (result)
+print ('tetst')
