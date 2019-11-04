7b1cd22761875b3307447a6c55b21ec834e06a50
Билолиддин Корабойев <b.koraboyev@napm.uz>
2019-11-04 10:31:50 +0500

    first commitss

--- soaptest.py
+++ soaptest.py
@@ -1,5 +1,5 @@
 from suds.client import Client
-url = 'https://ips.gov.uz:443/mediate/ips/STC/GetTinbyPasNum?wsdl'
+url = 'ips.gov.uz:443/mediate/ips/STC/GetTinbyPasNum?wsdl'
 client = Client(url)
 print (client)
 
